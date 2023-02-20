cb58b1996484f2cd5751e4712de1ffe00dd4c36e
KenJeo348 <jeongk@middleton.school.nz>
2023-02-21 12:16:15 +1300

    Commit

--- 03_taxi_trips.py
+++ 03_taxi_trips.py
@@ -1,22 +1,29 @@
-driver_name = input("Driver name:")
-start_trip = ""
-trip_amount = 0
+driver_name = input("Enter driver's name:")
+
 total_time = 0
-total_price = 0
+total_income = 0
+BASE_COST = 10
+PER_MINUTE_COST = 2
+start_trip = "y"
+trip_amount = 0
 
-while start_trip != "no":
+while start_trip != "n":
+    trip_time = int(input("How long did the trip take, (in minutes):"))
+    trip_amount = trip_amount + 1
+    total_time = total_time + trip_time
+    trip_cost = BASE_COST + trip_time * PER_MINUTE_COST
+    total_income = total_income + trip_cost
+    print(f"The cost of this trip was ${trip_cost}")
 
-    taxi_time = 0
-    start_trip = input("Would you like to start a new trip:")
+    start_trip = input("Would you like to start a new trip, (y or n):")
 
-    if start_trip == "yes":
-        taxi_time = int(input("How long was the taxi ride, (minutes):"))
-        trip_amount = trip_amount + 1
-        taxi_price = taxi_time * 2 + 10
-        total_time = total_time + taxi_time
-        total_price = total_price + taxi_price
-    elif start_trip == "no":
-        print()
-    else:
-        print("Please enter a valid answer.")
+average_time = total_time / trip_amount
+average_cost = total_income / trip_amount
+
+print(f"Taxi driver: {driver_name}\n"
+      f"Total time for all trips: {total_time} minutes.\n"
+      f"Average time of all trips: {average_time:.2f} minutes.\n"
+      f"Total income for the day: ${total_income:.2f} dollars. \n"
+      f"Average cost of all trips: ${average_cost:.2f}")
+
 
